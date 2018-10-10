840e3b0b0f164e8721121d0483291e2b41c1b38a
David Petticrew <cardassianscot@gmail.com>
2018-10-10 13:20:51 +0100

    Allow you to specify the characters to turn led on/off

--- ArduinoControl/Form1.Designer.cs
+++ ArduinoControl/Form1.Designer.cs
@@ -37,6 +37,10 @@
             this.btnOff = new System.Windows.Forms.Button();
             this.btnToggle = new System.Windows.Forms.Button();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -122,11 +126,49 @@
             this.hScrollBar1.Value = 90;
             this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "LED on character";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "LED off character";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 35);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(272, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 35);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1283, 842);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.btnToggle);
             this.Controls.Add(this.btnOff);
@@ -156,6 +198,10 @@
         private System.Windows.Forms.Button btnOff;
         private System.Windows.Forms.Button btnToggle;
         private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- ArduinoControl/Form1.cs
+++ ArduinoControl/Form1.cs
@@ -16,6 +16,8 @@ namespace ArduinoControl
         Arduino arduino;
 
         bool ledOn = false;
+        string ledOnKey = "L";
+        string ledOffKey = "l";
 
         public Form1()
         {
@@ -24,16 +26,18 @@ namespace ArduinoControl
             arduino = new Arduino();
             arduino.addMenu(arduinoToolStripMenuItem);
             arduino.addStatusStrip(sslConnection, sslMessage);
+            textBox1.Text = ledOnKey;
+            textBox2.Text = ledOffKey;
         }
 
         private void btnOn_Click(object sender, EventArgs e)
         {
-            arduino.write("L");
+            arduino.write(ledOnKey);
         }
 
         private void btnOff_Click(object sender, EventArgs e)
         {
-            arduino.write("l");
+            arduino.write(ledOffKey);
         }
 
         private void btnToggle_Click(object sender, EventArgs e)
@@ -41,13 +45,13 @@ namespace ArduinoControl
             if (ledOn)
             {
                 btnToggle.BackColor = SystemColors.Control;
-                arduino.write("l");
+                arduino.write(ledOffKey);
                 ledOn = false;
             }
             else
             {
                 btnToggle.BackColor = Color.Red;
-                arduino.write("L");
+                arduino.write(ledOnKey);
                 ledOn = true;
             }
         }
@@ -55,6 +59,16 @@ namespace ArduinoControl
         private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
             arduino.write("a" + hScrollBar1.Value + ";");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ledOnKey = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            ledOffKey = textBox2.Text;
         }
     }
 }
