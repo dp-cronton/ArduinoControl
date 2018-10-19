47a861efacb3ebeadf15562228f3a02e0493f47b
David Petticrew <cardassianscot@gmail.com>
2018-10-19 08:12:19 +0100

    lower case

--- ArduinoControl/Form1.Designer.cs
+++ ArduinoControl/Form1.Designer.cs
@@ -126,18 +126,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(69, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 29);
+            this.label1.Size = new System.Drawing.Size(197, 29);
             this.label1.TabIndex = 7;
-            this.label1.Text = "LED On character";
+            this.label1.Text = "LED on character";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(69, 209);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(201, 29);
+            this.label2.Size = new System.Drawing.Size(196, 29);
             this.label2.TabIndex = 8;
-            this.label2.Text = "LED Off character";
+            this.label2.Text = "LED off character";
             // 
             // textBox1
             // 
